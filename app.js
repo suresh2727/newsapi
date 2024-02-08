3c31aa50432c53676560be8e6dbd8ed1d72b66d0
suresh mistry <52049092+suresh2727@users.noreply.github.com>
2024-02-08 21:27:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+const apikey = "4ec3fc431c9a423d90bcc5aaa2832282";
+const apiurl =
+  "https://newsapi.org/v2/top-headlines?country=in&category=technology";
+
+const searchbtn = document.querySelector(".btn");
+const newsImage = document.querySelector(".new-img");
+const newsTitle = document.querySelector(".news-title");
+const newsDetails = document.querySelector(".news-details");
+const discon = document.querySelector(".news-card");
+async function processData() {
+  // Await the result of the asynchronous operation
+  const response = await fetch(apiurl + `&apiKey=${apikey}`);
+  if (response.status == 200) {
+    document.querySelector(".news-card").style.display = "block";
+    var data = await response.json();
+    console.log(data);
+    var articlesnumber = Math.floor(Math.random(parseInt(data.articles)) * 20);
+
+    document.querySelector(".news-title").innerHTML =
+      data.articles[articlesnumber].author;
+    document.querySelector(".news-details").innerHTML =
+      data.articles[articlesnumber].description;
+
+    var image = document.getElementById("news-img");
+    image.src = data.articles[articlesnumber].urlToImage;
+  } else {
+    console.log("hello");
+    document.querySelector(".news-card").style.display = "none";
+    alert("Problem in the API");
+  }
+
+  // Further processing with the received data
+}
+searchbtn.addEventListener("click", () => {
+  processData();
+  console.log("btn clicked");
+});
